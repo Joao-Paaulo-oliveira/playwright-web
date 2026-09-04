6d9bff2b7336b6a0ab5658b6540a8b05ca7bde47
João Paulo <seuemail@exemplo.com>
2026-09-04 16:24:58 -0300

    chore: configura evidencias de falha no playwright

--- playwright.config.js
+++ playwright.config.js
@@ -39,8 +39,10 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL,
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    /* Collect evidence only when a test fails. */
+    trace: "retain-on-failure",
+    screenshot: "only-on-failure",
+    video: "off",
   },
 
   /* Configure projects for major browsers */
